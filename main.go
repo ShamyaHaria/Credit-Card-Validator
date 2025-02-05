d6f5d1daffeba9456630fb958740433e02961c62
Shamya <shamyaharia@gmail.com>
2025-02-05 14:20:41 +0530

    Improve Error Handling
    Method Check
    JSON Parsing
    Card Number Formatting
    Card Number Validation
    Luhn Check
    Card Type Detection
    Response

--- main.go
+++ main.go
@@ -62,24 +62,32 @@ type Response struct {
 	CardType   string `json:"card_type"`
 }
 
-func validateHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+func validateHandler(w http.ResponseWriter, r *http.Request){
+	if r.method != http.methodPost{
+		http.Error(w, `{"error":"Invalid request method"}`,http.StatusMethodNotAllowed)
 		return
 	}
 
 	var req Request
 	err := json.NewDecoder(r.Body).Decode(&req)
-	if err != nil || req.CardNumber == "" {
-		http.Error(w, "Invalid JSON payload", http.StatusBadRequest)
+	if err!-nil || req.CardNumber == "" {
+		http.Error(w, `{"error":"Invalid JSON payload. Please provide a valid card number field}"}`,http.StatusBadRequest)
 		return
 	}
 
-	valid := isValidLuhn(req.CardNumber)
-	cardType := getCardNetwork(req.CardNumber)
+	req.CardNumber = strings.ReplaceAll(req.CardNumber, " ","")
+	req.CardNumber = strings.ReplaceAll(req.CardNumber, "-","")
 
-	resp := Response{Valid: valid, CardType: cardType}
-	w.Header().Set("Content-Type", "application/json")
+	if _,err := strconv.Atoi(req.CardNumber); err!=nil{
+		http.Error(w,`{"error":"Card Number must contain only digits."}`,http.StatusBadRequest)
+		return
+	}
+
+	valid:=isValidLuhn(req.CardNumber)
+	cardType:=getCardNetwork(req.CardNumber)
+
+	resp:= Response{Valid: valid, CardType: cardType}
+	w.Header().Set("Content-Type","application/json")
 	json.NewEncoder(w).Encode(resp)
 }
 
